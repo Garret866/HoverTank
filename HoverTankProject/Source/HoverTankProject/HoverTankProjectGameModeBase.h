306a0834a68afc3b98f929a3200c62604eac18bb
Boaz Yakubov <garret866@gmail.com>
2019-09-11 21:30:01 +0300

    New Git

---
+++ HoverTankProject/Source/HoverTankProject/HoverTankProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HoverTankProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HOVERTANKPROJECT_API AHoverTankProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
